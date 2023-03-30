7c0c2d1ec5a6efb1d7772d064f264f6077146390
ccozens <ccozens@gmail.com>
2023-03-30 10:50:58 +0100

    2 h 23 m, create, update and delete methods implemented

--- api/views.py
+++ api/views.py
@@ -26,12 +26,47 @@ def getRoutes(request):
 
 @api_view(["GET"])
 def getNotes(request):
-    notes = Note.objects.all()
-    serializer = NoteSerializer(notes, many=True) # many=True because we are serializing multiple objects. Will return back a queryset
-    return Response(serializer.data) # serializer is an object so this returns the data to frontend
+    notes = Note.objects.all().order_by("-updated")  # orders by most recently updated
+    serializer = NoteSerializer(
+        notes, many=True
+    )  # many=True because we are serializing multiple objects. Will return back a queryset
+    return Response(
+        serializer.data
+    )  # serializer is an object so this returns the data to frontend
+
 
 @api_view(["GET"])
 def getNote(request, pk):
     note = Note.objects.get(id=pk)
     serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["PUT"])
+def updateNote(request, pk):
+    data = request.data  # get json data
+    note = Note.objects.get(id=pk)  # get note
+    serializer = NoteSerializer(
+        instance=note, data=data
+    )  # convert json to Python data form
+    if serializer.is_valid():  # check data valid
+        serializer.save()  # save returns an object instance, based on the validated data.
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+def deleteNote(request, pk):
+    note = Note.objects.get(id=pk)
+    note.delete()
+    return Response("Note deleted")
+
+
+@api_view(["POST"])
+def createNote(request):
+    data = request.data  # json data
+    note = Note.objects.create(
+        body=data["body"]  # updated and created fields automatically generated
+    )
+    serializer = NoteSerializer(note, many=False)
+ 
     return Response(serializer.data)
--- ivy_notes_django_tutorial/settings.py
+++ ivy_notes_django_tutorial/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "api.apps.ApiConfig",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.common.CommonMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -123,3 +126,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  # allow localhost://3000
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+ALLOWED_HOSTS = ["*"]
